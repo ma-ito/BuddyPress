cf96c4045ddc815106d12cb322cadb50e5067470
ma-ito <ito.masahiro@cij.co.jp>
2013-08-29 16:31:15 +0900

    ユーザ追加時の配信メール内容変更

--- bp-custom.php
+++ bp-custom.php
@@ -20,6 +20,7 @@ function wp_new_user_notification($user_id, $plaintext_pass = '') {
 
 	$user_login = stripslashes($user->user_login);
 	$user_email = stripslashes($user->user_email);
+	$nice_name = stripslashes($user->user_nicename);
 
 	// The blogname option is escaped with esc_html on the way into the database in sanitize_option
 	// we want to reverse this for the plain text arena of emails.
@@ -34,13 +35,25 @@ function wp_new_user_notification($user_id, $plaintext_pass = '') {
 	if ( empty($plaintext_pass) )
 		return;
 
-	$message  = sprintf('%sのアカウントを作成しました。', $blogname) . "\r\n\r\n";
-	$message .= sprintf(__('Username: %s'), $user_login) . "\r\n";
-	$message .= sprintf(__('Password: %s'), $plaintext_pass) . "\r\n\r\n";
-	$message .= '下記のURLにアクセスして、パスワードとプロフィールの変更をお願いします。' . "\r\n";
-	$message .= wp_login_url() . "\r\n\r\n";
-	$message .= "みなさまの情報共有に役立てていただければ幸いです。\r\n\r\n";
-	$message .= "-- \r\n" . sprintf('%s運営チーム一同', $blogname);
+	$message =
+		'クローバーカフェのアカウントを作成しました。'. "\r\n" .
+		'みなさまの情報共有に役立てていただければ幸いです。' . "\r\n\r\n" .
+		'------------------------------------------------' . "\r\n" .
+		sprintf(__('Username: %s'), $user_login) . "\r\n" .
+		sprintf(__('Password: %s'), $plaintext_pass) . "\r\n\r\n" .
+		'下記のURLにアクセスしてログインしてください。' . "\r\n" .
+		site_url() . "\r\n" .
+		'------------------------------------------------' . "\r\n\r\n" .
+		'▽パスワードを変更する' . "\r\n" .
+		site_url() . '/members/' . $nice_name . '/settings/' . "\r\n\r\n" .
+		'▽プロフィールを変更する' . "\r\n" .
+		site_url() . '/members/' . $nice_name . '/profile/' . "\r\n\r\n" .
+		'------------------------------------------------' . "\r\n" .
+		'本メールにお心当たりがない場合は、恐れ入りますが' . "\r\n" .
+		'下記の連絡先までご連絡ください。' . "\r\n" .
+		'------------------------------------------------' . "\r\n\r\n" .
+		"-- \r\n" . 'クローバーカフェ運営チーム一同' . "\r\n" .
+		'連絡先：' . get_option('admin_email') . "\r\n";
 
 	wp_mail($user_email, 'アカウント登録のお知らせ', $message);
 
