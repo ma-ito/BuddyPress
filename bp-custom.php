a44107d3f9e13a9e0e7c9f60c2eb1765fd92c55e
ma-ito <ito.masahiro@cij.co.jp>
2014-10-05 15:52:28 +0900

    open sansフォントのローカル参照化

--- bp-custom.php
+++ bp-custom.php
@@ -15,17 +15,31 @@ add_filter( 'show_admin_bar', '__return_false' );
 /* enable old theme directory */
 add_filter( 'bp_do_register_theme_directory', '__return_true' );
 
-// rel="shortlink"
+/* header cleanup */
+/* rel="shortlink" */
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
 
-// rel="next"、rel="prev"
+/* rel="next"、rel="prev" */
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10 );
 
+/* recent_comments_style */
 function remove_recent_comments_style() {
 	global $wp_widget_factory;
 	remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
 }
 add_action( 'widgets_init', 'remove_recent_comments_style' );
+
+/* Replace Open Sans with a local copy */
+/* requires 'Local Open Sans' plugin */
+function disable_load_opensans ( &$styles ) {
+	$styles->remove( 'open-sans' );
+	if ( defined( 'LOCAL_OPEN_SANS_URL' ) ) {
+		$styles->add( 'open-sans', LOCAL_OPEN_SANS_URL . 'open-sans.css' );
+	} else {
+		$styles->add( 'open-sans', null );
+	}
+}
+add_action( 'wp_default_styles', 'disable_load_opensans' );
 
 /*
  * password strength meter
