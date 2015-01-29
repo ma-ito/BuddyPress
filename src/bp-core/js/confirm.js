764530136a81218a1385866d86304a9703256f40
ma-ito <ito.masahiro@cij.co.jp>
2015-01-30 00:55:43 +0900

    確認ダイアログの変更

--- src/bp-core/js/confirm.js
+++ src/bp-core/js/confirm.js
@@ -2,7 +2,7 @@
 /* global BP_Confirm */
 
 jQuery( document ).ready( function() {
-	jQuery( 'ul:not(#activity-stream) a.confirm').click( function() {
+	jQuery('a.confirm').not('.acomment-delete, .delete-activity').click( function() {
 		if ( confirm( BP_Confirm.are_you_sure ) ) {
 			return true;
 		} else {
